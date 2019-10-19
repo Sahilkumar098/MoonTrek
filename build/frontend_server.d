13a2f1d47c4503cf3407b60f88c2094e466bdc05
pearlyadav <yadavpearl2001@gmail.com>
2019-10-19 02:29:56 -0700

    updated and added navigation bar

--- build/frontend_server.d
+++ build/frontend_server.d
@@ -1,1 +1,1 @@
-frontend_server.d: C:\src\flutter\bin\cache\artifacts\engine\windows-x64\frontend_server.dart.snapshot
+frontend_server.d: C:\Users\ADMIN\Downloads\flutter_windows_v1.9.1+hotfix.5-stable\flutter\bin\cache\artifacts\engine\windows-x64\frontend_server.dart.snapshot
